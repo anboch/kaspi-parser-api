54efd6a0782310bf742baea7e8d61ad90efa73cd
anboch <a.g.bochka@gmail.com>
2023-01-20 11:31:16 +0600

    test: add e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { stopMongoInMemoryServer } from '../src/configs/mongo.config';
+import { validationPipeOptions } from '../src/configs/validation-pipe.config';
+
+jest.setTimeout(15000);
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe(validationPipeOptions));
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/parse-product/:kaspiId (GET)- failed - wrong kaspiId', async () => {
+    const kaspiId = '100692388a';
     return request(app.getHttpServer())
-      .get('/')
+      .get(`/parse-product/${kaspiId}`)
+      .expect(400)
+      .then(({ body }: request.Response) => {
+        expect(body.message[0]).toBe('kaspiId must be a number string');
+      });
+  });
+
+  it('/parse-product/:kaspiId (GET)- success - Should return title and price of a product', async () => {
+    const kaspiId = '100692388';
+    return request(app.getHttpServer())
+      .get(`/parse-product/${kaspiId}`)
       .expect(200)
-      .expect('Hello World!');
+      .then(({ body }: request.Response) => {
+        expect(body.title).toBeDefined();
+        expect(body.price).toBeGreaterThan(0);
+      });
+  });
+
+  afterAll(async () => {
+    await stopMongoInMemoryServer();
+    await app.close();
   });
 });
