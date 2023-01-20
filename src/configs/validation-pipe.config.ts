10311120e2da42afd7f37dd1f855ef1469cd1df2
anboch <a.g.bochka@gmail.com>
2023-01-20 11:30:10 +0600

    feat: add ValidationPipe

---
+++ src/configs/validation-pipe.config.ts
@@ -0,0 +1,7 @@
+import { ValidationPipeOptions } from '@nestjs/common';
+
+export const validationPipeOptions: ValidationPipeOptions = {
+  whitelist: true,
+  forbidNonWhitelisted: true,
+  transform: true,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { validationPipeOptions } from './configs/validation-pipe.config';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe(validationPipeOptions));
+  await app.listen(PORT);
 }
 bootstrap();
